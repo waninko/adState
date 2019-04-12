c68ea9f3807d00c0fde26d3ad9381d2fbae49838
waninko <43968374+waninko@users.noreply.github.com>
2019-04-12 12:20:10 +0200

    draft of loadData schema

--- adstate/src/components/LoadData.vue
+++ adstate/src/components/LoadData.vue
@@ -1,10 +1,29 @@
 <template>
+<div id="wrapper">  
+  <div id="menu"></div>
   
+  
+  <div id="main">
+    <button id="loadDataButton">Load Data</button>
+    <p> First Name: </p>
+    <input type="text">
+
+    <p> Last Name: </p>
+    <input type="text">
+
+    <p> Email: </p>
+    <input type="text">
+    <button id="saveButton"> Save </button> 
+
+    </div>
+
+
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'LoadData',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
